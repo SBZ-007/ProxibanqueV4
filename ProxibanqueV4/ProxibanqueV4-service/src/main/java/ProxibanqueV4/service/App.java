415362a98f243d01d628671dafc3bb90c2535aba
SBZ-007 <benazouz.salaheddine@hotmail.fr>
2018-05-18 17:09:49 +0200

    Mise en place du squelette ProxibanqueV4

---
+++ ProxibanqueV4/ProxibanqueV4-service/src/main/java/ProxibanqueV4/service/App.java
@@ -0,0 +1,13 @@
+package ProxibanqueV4.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
